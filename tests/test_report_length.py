3085449c42a776c66bf2b32351a070b3596f1dbc
kate.dollete <cfdollete@up.edu.ph>
2023-04-25 08:33:45 +0100

    Refactor report_length tests

--- tests/test_report_length.py
+++ tests/test_report_length.py
@@ -1,6 +1,22 @@
 from lib.report_length import *
 
-def test_report_length_returns_correct_length():
+"""
+Reports a length of zero, when passed an empty string.
+"""
+
+def test_report_length_returns_zero():
     assert report_length("") == "This string was 0 characters long."
-    assert report_length("Hello world!") == "This string was 12 characters long."
+
+"""
+Reports the correct length when passed a single word.
+"""
+
+def test_returns_correct_length_for_single_word():
+    assert report_length("Hello") == "This string was 5 characters long."
+
+"""
+Reports the correct length when passed multiple words.
+"""
+
+def test_returns_corrent_length_for_multiple_words():
     assert report_length("Welcome to the Makers bootcamp!") == "This string was 31 characters long."
